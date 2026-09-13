d1c9270a472df5bc96e249a49f6fc2be93bb70d0
Pritish <maitypritish28@gmail.com>
2026-09-13 11:18:58 +0530

    change the sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,11 +1,11 @@
 export default function sitemap() {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || "https://inhistime.example.com";
+  const base = process.env.NEXT_SITE_URL || "https://inhistime.example.com";
   return [
     {
       url: base,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 1
-    }
+      priority: 1,
+    },
   ];
 }
